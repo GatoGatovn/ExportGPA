09976df2f153ddafefd88f80b744e1fa4fb7fb71
GatoGatovn <124536695+GatoGatovn@users.noreply.github.com>
2023-02-09 23:51:51 +0700

    Update and rename Tạo Điểm TBHT version3.user.js to CreatGPA.user.js

--- CreatGPA.user.js
+++ CreatGPA.user.js
@@ -37,4 +37,4 @@ let newCell2 = newRow.insertCell(1);
 newCell2.colSpan = 3;
 newCell2.textContent = "Điểm trung bình học tập: " + averageScore.toFixed(4);
 }
-setTimeout(addscore, 2000);
+setTimeout(addscore, 3000);
