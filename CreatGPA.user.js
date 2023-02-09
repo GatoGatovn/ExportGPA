5ef4f26517a1339c52110de1dee74bc67964aeba
GatoGatovn <124536695+GatoGatovn@users.noreply.github.com>
2023-02-09 23:54:56 +0700

    Update CreatGPA.user.js

--- CreatGPA.user.js
+++ CreatGPA.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Tạo Điểm TBHT
+// @name         Tạo GPA
 // @namespace    https://congdaotao.tmu.edu.vn/student
 // @version      4
 // @description  tạo GPA cho TMUer
