ecde6dbf6acc984ba3abc70c080dfffca4c002c7
GatoGatovn <124536695+GatoGatovn@users.noreply.github.com>
2023-06-19 00:59:03 +0700

    Update AddGPA.user.js

--- AddGPA.user.js
+++ AddGPA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AddGPA
 // @namespace    https://congdaotao.tmu.edu.vn
-// @version      4
+// @version      4.5
 // @description  :)
 // @author       TMUer Học dốt nên chưa có ny T.T
 // @match        https://congdaotao.tmu.edu.vn/*
